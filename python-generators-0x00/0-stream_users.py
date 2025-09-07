c388bfab7e77f922a0ff6617d10ec87030727cee
Yonas Baysasaw <yonas.baysasaw.kibret@gmail.com>
2025-09-07 23:33:03 +0300

    Update 0-stream_users.py

--- python-generators-0x00/0-stream_users.py
+++ python-generators-0x00/0-stream_users.py
@@ -1,18 +1,10 @@
 #!/usr/bin/python3
-"""
-Module: 0-stream_users
-Objective: Create a generator that streams rows from an SQL database one by one.
-"""
 
 import mysql.connector
 
 
 def stream_users():
-    """
-    Generator function that streams rows from user_data table one by one.
-    Yields:
-        tuple: A single row from the user_data table.
-    """
+
     try:
         # Connect directly to ALX_prodev database
         connection = mysql.connector.connect(
