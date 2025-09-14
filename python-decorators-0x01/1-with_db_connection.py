899f5de3dddb226d6bcba34c16403f177b140a4c
yonas-baysasaw <yonas.baysasaw.kibret@gmail.com>
2025-09-14 21:34:06 +0300

    Handle Database Connections with a Decorator

---
+++ python-decorators-0x01/1-with_db_connection.py
@@ -0,0 +1,30 @@
+import sqlite3
+import functools
+
+
+def with_db_connection(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        conn = sqlite3.connect("users.db")
+        try:
+            result = func(conn, *args, **kwargs)
+            conn.commit()
+            return result
+        except Exception as e:
+            conn.rollback()
+            raise e
+        finally:
+            conn.close()
+    return wrapper
+
+
+@with_db_connection
+def get_user_by_id(conn, user_id):
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM users WHERE id = ?", (user_id,))
+    return cursor.fetchone()
+
+
+# Fetch user by ID with automatic connection handling
+user = get_user_by_id(user_id=1)
+print(user)
